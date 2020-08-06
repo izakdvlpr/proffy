d7245b1cb4099f7b282f59e4e914b1f164d5f415
zevdvlpr <zevdvlpr@gmail.com>
2020-08-06 14:16:30 -0300

    create input, select and textarea component | create form page

---
+++ web/src/components/Select/index.tsx
@@ -0,0 +1,31 @@
+import React, { SelectHTMLAttributes } from "react";
+
+import "./styles.css";
+
+interface SelectProps extends SelectHTMLAttributes<HTMLSelectElement> {
+  name: string;
+  label: string;
+  options: Array<{
+    value: string;
+    label: string;
+  }>;
+}
+
+const Select: React.FC<SelectProps> = ({ name, label, options, ...rest }) => (
+  <div className="select-block">
+    <label htmlFor={name}>{label}</label>
+    <select id={name} {...rest}>
+      <option value="" disabled selected hidden>Selecione uma opção</option>
+      
+      {options.map((option) => {
+        return (
+          <option key={option.value} value={option.value}>
+            {option.value}
+          </option>
+        );
+      })}
+    </select>
+  </div>
+);
+
+export default Select;
