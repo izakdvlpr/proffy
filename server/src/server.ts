133e2fd7e9510e90c539c31e86286ccad94cd24a
zevdvlpr <zevdvlpr@gmail.com>
2020-08-05 21:33:09 -0300

    create classes controller

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,9 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
+app.use(routes);
 
-app.get('/', (req, res) => {
-  return res.json({ message: 'Hello World' });
-});
-
-app.listen(4001);
+app.listen(3333);
